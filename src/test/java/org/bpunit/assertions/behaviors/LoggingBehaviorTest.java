c5e430f2504d4c15daa8ba2e50873cabf3bd62e4
Allon Mureinik <mureinik@gmail.com>
2016-08-20 11:37:52 +0300

    Use Theories in LoggingBehaviorTest
    Reduce codebloating by running with Theories.

--- src/test/java/org/bpunit/assertions/behaviors/LoggingBehaviorTest.java
+++ src/test/java/org/bpunit/assertions/behaviors/LoggingBehaviorTest.java
@@ -1,29 +1,24 @@
 package org.bpunit.assertions.behaviors;
 
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 /**
  * A basic test case for the {@link LoggingBehavior} class.
  * At this point, it just tests the behavior does not fail.
  * If in the future some mocking mechanism is introduced to the project, this test should be expanded
  */
+@RunWith(Theories.class)
 public class LoggingBehaviorTest {
-    @Test
-    public void behaveDefault() {
-        Behavior behavior = new LoggingBehavior();
-        behavior.behave("message", null);
-    }
 
-    @Test
-    public void behaveNoStackTrace() {
-        Behavior behavior = new LoggingBehavior(false);
-        behavior.behave("message", null);
-    }
+    @DataPoints
+    public static LoggingBehavior[] BEHAVIORS =
+            {new LoggingBehavior(), new LoggingBehavior(true), new LoggingBehavior(false)};
 
-
-    @Test
-    public void behaveWithStackTrace() {
-        Behavior behavior = new LoggingBehavior(true);
+    @Theory
+    public void behavae(LoggingBehavior behavior) {
         behavior.behave("message", null);
     }
 }
